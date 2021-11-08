d6ae4a3e91656cf83c80e415a0f8994c84037ad6
Wilder Valverde <wvalverder@gmail.com>
2021-11-08 15:21:59 -0500

    Introducimos los calculos del perimetro y area del Cuadrado , Traingulo y Circulos por JS

---
+++ figuras.js
@@ -0,0 +1,56 @@
+//codigo del Cuadrado
+
+console.group("Cuadrados");
+
+const ladoCuadrado = 5;
+
+console.log("Lado del Cuadrado miden: " + ladoCuadrado + "cm")
+
+const perimetroCuadrado = ladoCuadrado *4;
+
+console.log("El perimetro es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+
+console.log("El area del cuadrado es: " + areaCuadrado + "cm*cm");
+
+console.groupEnd();
+
+//codigo del Triangulo.
+
+console.group("Triangulos");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.6;
+
+console.log("Los lados del Triangulo miden: " + ladoTriangulo1 + "cm ," + ladoTriangulo2 + "cm ," + baseTriangulo + "cm")
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+
+console.log("El perimetro del triangulo es: " + perimetroTriangulo + "cm");
+
+const areaTrinagulo = baseTriangulo*alturaTriangulo/2;
+
+console.log("El area del triangulo es: " + areaTrinagulo + "cm*cm");
+
+console.groupEnd();
+
+console.group("Circulos");
+
+const radioCirculo = 4;
+
+const pi = 3.141592;
+
+console.log("El radio del Circulo es: " + radioCirculo + "cm");
+
+const longitudCircular = 2*pi*radioCirculo;
+
+console.log("La longitud del Circulo es: " + longitudCircular + "cm")
+
+const areaCircular = pi*radioCirculo*radioCirculo;
+
+console.log("El area del Circulo es: " + areaCircular + "cm*cm");
+console.groupEnd();
+
