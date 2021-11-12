1f3b93ff52990c8002c39a0dc7f6612f0848e00a
Wilder Valverde <wvalverder@gmail.com>
2021-11-12 18:29:35 -0500

    Se agrego mediana html y js

---
+++ mediana.js
@@ -0,0 +1,36 @@
+const lista1 = [ 100 , 200 , 500 , 800, 4000000];
+
+const mitadLista1 = parseInt(lista1.length/2);
+
+function esPar(numerito) {
+    if (numerito % 2 === 0) {
+        return true;
+        
+    } else {
+        return false;
+    }
+}
+
+function calcularMediaAritmetica(lista) {
+    let sumaLista = 0;
+    for(let i = 0; i < lista.length; i++) {
+      sumaLista = sumaLista + lista[i];
+    }
+    
+    const promedioLista = sumaLista/lista.length;
+      return promedioLista;
+}
+
+let mediana;
+
+
+if (esPar(lista1.length)) {
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioelemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+
+    mediana = promedioelemento1y2;
+} else {
+    mediana = lista1[mitadLista1];
+}
---
+++ promedio.js
@@ -0,0 +1,10 @@
+
+function calcularMediaAritmetica(lista) {
+    let sumaLista = 0;
+    for(let i = 0; i < lista.length; i++) {
+      sumaLista = sumaLista + lista[i];
+    }
+    
+    const promedioLista = sumaLista/lista.length;
+      return promedioLista;
+}
